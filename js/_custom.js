083505bc61754337352be85c5466fe11e8230a02
Enrage <enrajet@gmail.com>
2019-10-22 03:44:32 +0300

    Change render method

--- js/_custom.js
+++ js/_custom.js
@@ -3,9 +3,10 @@ document.addEventListener("DOMContentLoaded", function() {
 	getFilms();
 
 	function getFilms(searchStr = '') {
-		let apiUrl = 'https://swapi.co/api/films/';
+		const apiUrl = 'https://swapi.co/api/films/';
 		fetch(apiUrl).then(response => response.json())
 		.then((data) => {
+			let html = '';
 			for (let i = 0; i < data.results.length; i++) {
 				if (searchStr !== '' && searchStr !== undefined) {
 					if (data.results[i].title.toLowerCase().indexOf(searchStr.toLowerCase()) < 0) {
@@ -13,18 +14,17 @@ document.addEventListener("DOMContentLoaded", function() {
 					}
 				}
 
-				$('.films_list')
-				.append($('<div/>', { class: 'film', id: 'film-' + i })
-					.append($('<div/>', { class: 'film_img' })
-						.append($('<img/>', { src: '', alt: data.results[i].title }))
-					)
-					.append($('<div/>', { class: 'film_desc' })
-						.append($('<h2/>', { class: 'film_title', text: data.results[i].title }))
-						.append($('<p/>', {class: 'film_producer', text: data.results[i].producer }))
-					)
-				)
+				html += `<div class="film" id="film-${i}">
+						<div class="film_img">
+							<img src="http://placehold.it/260x85?text=Placeholder" alt="${data.results[i].title}">
+						</div>
+						<div class="film_desc">
+							<h2 class="film_title">${data.results[i].title}</h2>
+							<p class="film_producer">${data.results[i].producer}</p>
+						</div>
+					</div>`;
 
-				let apiPhotoUrl = 'https://pixabay.com/api/?key=13469464-412b7bcd981b5a6c95e7f09b9&q=' + data.results[i].title + '&image_type=photo';
+				let apiPhotoUrl = `https://pixabay.com/api/?key=13469464-412b7bcd981b5a6c95e7f09b9&q=${data.results[i].title}&image_type=photo`;
 
 				fetch(apiPhotoUrl).then(response => response.json())
 				.then((data) => {
@@ -34,10 +34,13 @@ document.addEventListener("DOMContentLoaded", function() {
 					} else {
 						filmImgSrc = 'http://placehold.it/260x85?text=Placeholder';
 					}
-					$('#film-' + i).find('img').attr('src', filmImgSrc);
+
+					document.querySelector(`#film-${i} img`).setAttribute('src', filmImgSrc);
 				});
 
 			}
+
+			document.querySelector('.films_list').innerHTML = html;
 		});
 	}
 
